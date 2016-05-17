f742e4f2df8d35c2173f7ee0a4ccfffeac8721a8
Adam Franco <afranco@middlebury.edu>
2016-05-17 11:01:33 -0400

    Clean up `knife node cookbook versions` column widths.

--- node_cookbook_versions.rb
+++ node_cookbook_versions.rb
@@ -40,11 +40,11 @@ module NodeCookbookVersions
       end
 
       env_data = Chef::Environment.load(node.chef_environment)
-      columns = '%-15.15s %-15.15s %-20.20s %-10.10s %-10.10s %-20.20s'
+      columns = '%-15.15s %-15.15s %-25.25s %-10.10s %-10.10s %-20.20s'
 
       if config[:show_column_header]
         puts sprintf(columns, 'node', 'environment', 'cookbook', 'version', 'frozen?', 'env. constraint')
-        puts '--------------------------------------------------------------------------------------------------------------------'
+        puts '-------------------------------------------------------------------------------------------------'
       end
 
       for cookbook, version in node['cookbook_versions']
